bd80b3ed05bb8852be0fceb82517106ba8d2db38
João Bispo <joaobispo2077@gmail.com>
2021-05-01 18:50:53 -0300

    feat: learned lazy evaluation

---
+++ functional/lazy-evaluation/index.js
@@ -0,0 +1,35 @@
+// Lazy-evaluation ou lazy-load
+
+function eager(a, b) {
+
+  const final = Date.now() + 2500;
+  while (Date.now() < final) { }
+
+  const value = Math.pow(a, 3);
+  return value + b;
+}
+
+function lazy(a) {
+
+  const final = Date.now() + 2500;
+  while (Date.now() < final) { }
+
+  const value = Math.pow(a, 3);
+  return function (b) {
+    return value + b;
+  }
+}
+
+console.time('#1');
+console.log(eager(3, 100));
+console.log(eager(3, 200));
+console.log(eager(3, 300));
+console.timeEnd('#1');
+
+const lazy3 = lazy(3);
+
+console.time('#2');
+console.log(lazy3(100));
+console.log(lazy3(200));
+console.log(lazy3(300));
+console.timeEnd('#2');
